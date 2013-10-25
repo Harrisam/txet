cf453e822e504284a43f0a9dfc802ddaf4617899
Harrisam <samantha.s.harris@googlemail.com>
2013-10-25 10:54:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Goal is to create a simple Rails app with a form including a mobile number and a message.
+Using Twilio's API, send a text message to the number with the message from the form. 
+
+Possible extentions:
+
+- Add devise - user must be logged in to send message
+- A user has to buy credits to send messages. 1 credit = 1 message. 100 credits cost £10. Use stripe checkout for payment.
+- Push to Heroku!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Txet::Application.config.secret_key_base = 'f6edbe6e14ad408320bc995875a440520396cc342173608b88c8a9b4239635431165dfec1ecf5623760f6cd5e44da617dbdcb6ec4113828ba7b8c0d4203c423b'
